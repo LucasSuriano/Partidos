3b10f4abf7348c8d2b3a4039221b27e2ab49c590
DESKTOP-C7UC437\User <lucassuriano97@gmail.com>
2026-04-19 22:08:50 -0300

    Mejora para hacer un unico APK.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.partidoslujan.app',
   appName: 'Partidos Lujan',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'https://partidos-ruby.vercel.app/',
+    cleartext: true
+  }
 };
 
 export default config;
